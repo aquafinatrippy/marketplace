ac2694e9eb1b11e1ac32b95128d4e3682d40293e
Tanel Tekko <taneldeviis@hotmail.com>
2020-05-21 11:02:33 +0300

    Footer final design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,16 +7,7 @@
       justify="center"
       no-gutters
     >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-      >
-        {{ link }}
-      </v-btn>
+     
       <v-col
         class="primary lighten-2 py-4 text-center white--text"
         cols="12"
@@ -30,14 +21,6 @@
 <script>
   export default {
     data: () => ({
-      links: [
-        'Home',
-        'About Us',
-        'Team',
-        'Services',
-        'Blog',
-        'Contact Us',
-      ],
     }),
   }
 </script>
