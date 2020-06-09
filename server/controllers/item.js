bc1d9a9e29eff35266a544a2daa32d39f59d6a6e
Tanel Tekko <taneldeviis@hotmail.com>
2020-06-10 01:47:06 +0300

    backend saves pictures

--- server/controllers/item.js
+++ server/controllers/item.js
@@ -4,6 +4,13 @@ module.exports = {
   addItem: async (req, res) => {
     try {
     console.log(req.file)
+    // let itemData = {
+    //     Title: req.body.Title,
+    //     seller: req.body.seller,
+    //     description: req.body.description,
+    //     price: req.body.price,
+    //     item: req.file.filename
+    // }
       const item = new Item(req.body);
       await item.save();
       res.status(200).json(item);
