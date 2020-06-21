b4c980fe25b404edc0712e21ea5c45f5126ffd7f
Tanel Tekko <taneldeviis@hotmail.com>
2020-06-22 01:42:31 +0300

    upload vuex building, on progress

--- src/views/UploadSell.vue
+++ src/views/UploadSell.vue
@@ -1,10 +1,12 @@
 <template>
   <v-container>
-    <v-text-field required label="Item name"></v-text-field>
-    <v-textarea name="input-7-1" label="Item description" hint="Item description"></v-textarea>
-    <v-file-input label="File input" filled prepend-icon="mdi-camera"></v-file-input>
+    <v-text-field required label="Item name" v-model="name"></v-text-field>
+    <v-text-field required label="Item name" v-model="price"></v-text-field>
+    
+    <v-textarea name="input-7-1" label="Item description" hint="Item description" v-model="desc"></v-textarea>
+    <v-file-input label="File input" filled prepend-icon="mdi-camera"  v-model="file"></v-file-input>
     <div class="my-2">
-      <v-btn x-large color="primary" dark>Add to sale</v-btn>
+      <v-btn x-large color="primary" dark @click="postItem">Add to sale</v-btn>
     </div>
   </v-container>
 </template>
@@ -13,8 +15,30 @@
 export default {
   data: () => ({
     name: "",
-    desc: ""
-  })
+    desc: "",
+    file: "",
+    price: null
+  }),
+  methods: {
+    async postItem(){
+      try {
+         const user = this.$store.state.user.username
+      const d = {
+        Title: this.name,
+        seller: user,
+        description: this.desc,
+        price: this.price,
+        image: this.file,
+        
+      }
+      console.log(d)
+      console.log(user)
+      } catch (error) {
+        console.log(error)
+      }
+     
+    }
+  }
 };
 </script>
 
